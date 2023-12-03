0d69465ee83943cb27ff6b6be0323a981121f4c9
chams-tmar-org <chams.tmar@insat.ucar.tn>
2023-12-03 23:45:08 +0100

    first commit

---
+++ ASPCoreApplication2023DBfirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPCoreApplication2023DBfirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
